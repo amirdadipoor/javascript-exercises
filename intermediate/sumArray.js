ca7864fdfddd06de21a08f906078e38d964e03ef
shaghayeghbeyki78 <shaghayeghbeyki4@gmail.com>
2025-01-26 14:14:00 +0330

    solve sumArray

--- intermediate/sumArray.js
+++ intermediate/sumArray.js
@@ -11,5 +11,11 @@
  */
 
 // Write your solution here
-
+function sumArray(arr) {
+    var total = 0;
+    for  (var i = 0; i < arr.length; i++) {
+        total += arr[i];
+    }
+    return total;
+}
 module.exports = sumArray;
