1342641db1cc98a9729549f63e2505d921bdc5d0
shaghayeghbeyki78 <shaghayeghbeyki4@gmail.com>
2025-01-26 11:39:26 +0330

    solve factorial

--- intermediate/factorial.js
+++ intermediate/factorial.js
@@ -11,5 +11,12 @@
  */
 
 // Write your solution here
-
+function factorial(n) {
+    if (n < 0) {
+        return undefined;
+    } if (n == 0 || n==1) {
+        return 1;
+    }
+    return n * factorial(n - 1);
+}
 module.exports = factorial;
