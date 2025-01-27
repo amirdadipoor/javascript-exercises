a3186d4c98fc8b484482491d87789f0ea062b76c
shaghayeghbeyki78 <shaghayeghbeyki4@gmail.com>
2025-01-27 15:04:04 +0330

    Solve sumOfDigits

--- advanced/sumOfDigits.js
+++ advanced/sumOfDigits.js
@@ -9,5 +9,7 @@
  * sumOfDigits(123) should return 6 (1 + 2 + 3).
  * sumOfDigits(987) should return 24 (9 + 8 + 7).
  */
-
+function sumOfDigits(n) {
+    return n.toString().split('').reduce((sum, digit) => sum + parseInt(digit), 0);
+}
 module.exports = sumOfDigits;
