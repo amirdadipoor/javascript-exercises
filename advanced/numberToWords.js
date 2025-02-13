6ac1e8ed6d6d397b7b4a4916401f3f0fb4bb1ce6
Mahiar <mahiarnoori77@gmail.com>
2025-02-13 18:08:45 +0330

    solve numberto words

--- advanced/numberToWords.js
+++ advanced/numberToWords.js
@@ -12,4 +12,22 @@
 
 // Write your solution here
 
+function numberToWords(num) {
+    let ones = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    let teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    if (num < 10) {
+        return ones[num];    
+    } else if (num < 20) {
+        return teens[num - 10]; 
+    } else if (num < 100) {
+        return tens[Math.floor(num / 10)] + (num % 10 === 0 ? "" : " " + ones[num % 10]);
+    } else if (num < 1000) {
+        return ones[Math.floor(num / 100)] + " hundred" + (num % 100 === 0 ? "" : " " + numberToWords(num % 100));
+    }
+
+    return "Number too large!";
+}
+
 module.exports = numberToWords;
