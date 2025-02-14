052df8d37d10f2f5da3593e63f29784a7ab10d96
Mahiar <mahiarnoori77@gmail.com>
2025-02-14 14:17:41 +0330

    solve digitalclock

--- advanced/digitalClock.js
+++ advanced/digitalClock.js
@@ -19,4 +19,15 @@
  */
 
 
+function digitalClock() {
+    const now = new Date();
+    
+    let hours = now.getHours().toString().padStart(2, "0");
+    let minutes = now.getMinutes().toString().padStart(2, "0");
+    let seconds = now.getSeconds().toString().padStart(2, "0");
+
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    
+    document.getElementById("clock").textContent = timeString;
+}
 module.exports = digitalClock;
