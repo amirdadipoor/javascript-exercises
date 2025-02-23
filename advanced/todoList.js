89733ce3d6eef7d24b904d0231f3e6de82d2a4d7
Amir Dadipoor <amirdadipoor>
2025-02-23 23:07:04 +0330

    modify todo list

--- advanced/todoList.js
+++ advanced/todoList.js
@@ -1,27 +1,19 @@
-// /**
-//  * Problem:
-//  * Write a program to manage a to-do list.
-//  * The program should support adding tasks, removing tasks, and displaying all tasks.
-//  *
-//  * Your task:
-//  * Implement the functions `addTask`, `removeTask`, and `showTasks` to manage the to-do list.
-//  *
-//  * Example:
-//  * addTask("Buy groceries") should add a new task.
-//  * removeTask("Buy groceries") should remove the task.
-//  * showTasks() should return a list of all current tasks.
-//  */
+/**
+ * Problem:
+ * Write a program to manage a to-do list.
+ * The program should support adding tasks, removing tasks, and displaying all tasks.
+ *
+ * Your task:
+ * Implement the functions `addTask`, `removeTask`, and `showTasks` to manage the to-do list.
+ *
+ * Example:
+ * addTask("Buy groceries") should add a new task.
+ * removeTask("Buy groceries") should remove the task.
+ * showTasks() should return a list of all current tasks.
+ */
 
-const todoList = [];
-const addTask = task =>
-  task && typeof task === 'string' && !todoList.includes(task)
-    ? (todoList.push(task), `${task} added to the list.`)
-    : 'Error: Task cannot be empty or already exists.';
-const removeTask = task => {
-  const taskIndex = todoList.indexOf(task);
-  return taskIndex !== -1
-    ? (todoList.splice(taskIndex, 1), `${task} removed from the list.`)
-    : 'Error: Task not found.';
-};
+const todoList = new Set([]);
+const addTask = (task) => todoList.has(task) ? false : todoList.add(task) ;
+const removeTask = (task) => todoList.has(task) ? todoList.delete(task) : false;
 const showTasks = () => [...todoList];
 module.exports = { addTask, removeTask, showTasks }
